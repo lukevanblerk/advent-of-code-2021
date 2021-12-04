5647f3fe0f055bf44fd1d270ee4fe5293b2abde8
lukevanblerk <49236876+lukevanblerk@users.noreply.github.com>
2021-12-04 13:29:35 +0200

    Day 3 Part 1 solved *****

---
+++ AdventOfCode/Solvers/Day3/Day3Solver.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode.Solvers.Day3
+{
+    public class Day3Solver : ISolver
+    {
+        public string SolvePart1(Input input)
+        {
+            var diagnosticReport = input.AsArrayOfString();
+
+            var binaryGammaRate = CalculateGammaRate(diagnosticReport);
+            var gammaRate = Convert.ToInt32(binaryGammaRate, 2);
+            var binaryEpsilonRate = CalculateEpsilonFromGammaRate(binaryGammaRate);
+            var epsilonRate = Convert.ToInt32(binaryEpsilonRate, 2);
+
+            return (gammaRate * epsilonRate).ToString();
+        }
+
+        private string CalculateEpsilonFromGammaRate(string binaryGammaRate)
+        {
+            var epsilonRate = string.Empty;
+            foreach (var bit in binaryGammaRate)
+            {
+                epsilonRate += bit == '0' ? "1" : "0";
+            }
+            return epsilonRate;
+        }
+
+        public string SolvePart2(Input input)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private string CalculateGammaRate(string[] diagnosticReport)
+        {
+            string gammaRate = String.Empty;
+
+            for (int i = 0; i < diagnosticReport.First().Length; i++)
+            {
+                gammaRate += FindMostCommonChar(diagnosticReport, i);
+            }
+
+            return gammaRate;
+        }
+
+        private string FindMostCommonChar(string[] diagnosticReport, int i)
+        {
+            var zeros = 0;
+            var ones = 0;
+            foreach (var reportData in diagnosticReport)
+            {
+                if (reportData[i] == '0')
+                {
+                    zeros++;
+                }
+                else
+                {
+                    ones++;
+                }
+            }
+
+            return zeros > ones ? "0" : "1";
+        }
+    }
+}
---
+++ AdventOfCodeTests/Day3Tests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using AdventOfCode.Solvers;
+using AdventOfCode.Solvers.Day3;
+using NUnit.Framework;
+
+namespace AdventOfCodeTests
+{
+    public class Day3Tests
+    {
+        private ISolver _solver;
+
+        [SetUp]
+        public void Setup()
+        {
+            _solver = new Day3Solver();
+        }
+
+        [Test]
+        public void TestPart1()
+        {
+            // Arrange
+            var input = new Input(day: 3);
+            
+            // Act
+            var result = _solver.SolvePart1(input);
+            
+            // Assert
+            Console.WriteLine(result);
+        }
+
+        [Test]
+        public void TestPart2()
+        {
+            // Arrange
+            var input = new Input(day: 3);
+            
+            // Act
+            var result = _solver.SolvePart2(input);
+            
+            // Assert
+            Console.WriteLine(result);
+        }
+    }
+}
