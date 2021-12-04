4c2048953828c3148e550574f950e429e5ae6fd7
lukevanblerk <49236876+lukevanblerk@users.noreply.github.com>
2021-12-04 13:58:52 +0200

    Day 3 Part 3 solved ******

--- AdventOfCode/Solvers/Day3/Day3Solver.cs
+++ AdventOfCode/Solvers/Day3/Day3Solver.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode.Solvers.Day3
@@ -16,6 +17,19 @@ namespace AdventOfCode.Solvers.Day3
 
             return (gammaRate * epsilonRate).ToString();
         }
+        
+        public string SolvePart2(Input input)
+        {
+            var diagnosticReport = input.AsArrayOfString();
+
+            var binaryOxygenGeneratorRating = CalculateOxygenGeneratorRating(diagnosticReport);
+            var oxygenGeneratorRating = Convert.ToInt32(binaryOxygenGeneratorRating, 2);
+
+            var binaryCO2ScrubberRating = CalculateCO2ScrubberRating(diagnosticReport);
+            var co2ScrubberRating = Convert.ToInt32(binaryCO2ScrubberRating, 2);
+
+            return (oxygenGeneratorRating * co2ScrubberRating).ToString();
+        }
 
         private string CalculateEpsilonFromGammaRate(string binaryGammaRate)
         {
@@ -25,11 +39,6 @@ namespace AdventOfCode.Solvers.Day3
                 epsilonRate += bit == '0' ? "1" : "0";
             }
             return epsilonRate;
-        }
-
-        public string SolvePart2(Input input)
-        {
-            throw new System.NotImplementedException();
         }
 
         private string CalculateGammaRate(string[] diagnosticReport)
@@ -44,7 +53,7 @@ namespace AdventOfCode.Solvers.Day3
             return gammaRate;
         }
 
-        private string FindMostCommonChar(string[] diagnosticReport, int i)
+        private string FindMostCommonChar(IEnumerable<string> diagnosticReport, int i, string defaultBit = null)
         {
             var zeros = 0;
             var ones = 0;
@@ -60,7 +69,75 @@ namespace AdventOfCode.Solvers.Day3
                 }
             }
 
+            if (zeros == ones)
+            {
+                return defaultBit;
+            }
+
             return zeros > ones ? "0" : "1";
+        }
+
+        private string FindLeastCommonChar(IEnumerable<string> diagnosticReport, int i, string defaultBit = null)
+        {
+            var zeros = 0;
+            var ones = 0;
+            foreach (var reportData in diagnosticReport)
+            {
+                if (reportData[i] == '0')
+                {
+                    zeros++;
+                }
+                else
+                {
+                    ones++;
+                }
+            }
+
+            if (zeros == ones)
+            {
+                return defaultBit;
+            }
+
+            return zeros > ones ? "1" : "0";
+        }
+        
+        private string CalculateOxygenGeneratorRating(string[] diagnosticReport)
+        {
+            var filteredReport = diagnosticReport.ToList();
+            for (int i = 0; i < diagnosticReport.First().Length; i++)
+            {
+                var bit = FindMostCommonChar(filteredReport, i, "1");
+                filteredReport = FilterReportData(filteredReport, i, bit);
+
+                if (filteredReport.Count == 1)
+                {
+                    return filteredReport.First();
+                }
+            }
+
+            return string.Empty;
+        }
+
+        private string CalculateCO2ScrubberRating(string[] diagnosticReport)
+        {
+            var filteredReport = diagnosticReport.ToList();
+            for (int i = 0; i < diagnosticReport.First().Length; i++)
+            {
+                var bit = FindLeastCommonChar(filteredReport, i, "0");
+                filteredReport = FilterReportData(filteredReport, i, bit);
+
+                if (filteredReport.Count == 1)
+                {
+                    return filteredReport.First();
+                }
+            }
+
+            return string.Empty;
+        }
+
+        private List<string> FilterReportData(List<string> filteredReport, int i, string bit)
+        {
+            return filteredReport.Where(f => f[i].ToString() == bit).ToList();
         }
     }
 }
