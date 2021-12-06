89f76035e5d7a99e8365d4eb4e0b5600485f3b58
lukevanblerk <49236876+lukevanblerk@users.noreply.github.com>
2021-12-06 15:18:41 +0200

    Day 6 Part 2 solved 12*

--- AdventOfCode/Solvers/Day6/Day6Solver.cs
+++ AdventOfCode/Solvers/Day6/Day6Solver.cs
@@ -14,6 +14,37 @@ namespace AdventOfCode.Solvers.Day6
             
             return lanternFishes.Count().ToString();
         }
+        
+        public string SolvePart2(Input input)
+        { 
+            var inputFishes = input.AsText().Split(',').Select(i => Convert.ToInt32(i));
+            var daysLeftCount = new double[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+            foreach (var fishDaysLeft in inputFishes)
+            {
+                daysLeftCount[fishDaysLeft]++;
+            }
+
+            for (int i = 0; i < 256; i++)
+            {
+                var previousDayCounts = new double[9];
+                daysLeftCount.CopyTo(previousDayCounts, 0);
+
+                daysLeftCount[0] = previousDayCounts[1];
+                daysLeftCount[1] = previousDayCounts[2];
+                daysLeftCount[2] = previousDayCounts[3];
+                daysLeftCount[3] = previousDayCounts[4];
+                daysLeftCount[4] = previousDayCounts[5];
+                daysLeftCount[5] = previousDayCounts[6];
+                daysLeftCount[6] = previousDayCounts[7] + previousDayCounts[0];
+                daysLeftCount[7] = previousDayCounts[8];
+                daysLeftCount[8] = previousDayCounts[0];
+            }
+
+            var totalFish = daysLeftCount.Sum(d => d);
+
+            return totalFish.ToString();
+        }
 
         private void AddDays(int days, List<LanternFish> lanternFishes)
         {
@@ -38,12 +69,6 @@ namespace AdventOfCode.Solvers.Day6
                 lanternFishes.Add(new LanternFish(age));
             }
             return lanternFishes;
-        }
-
-
-        public string SolvePart2(Input input)
-        {
-            throw new System.NotImplementedException();
         }
     }
 }
